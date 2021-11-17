b5a8e0202b94e4ac04617d530af5f64ab7b4cfa3
GregDigital <68654973+GregDigital@users.noreply.github.com>
2021-11-17 22:25:39 +0100

    home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Page accueil</h1>
+    <div className="home">
+      <h1>Bienvenue sur <span>SportSee</span></h1>
+      <Link className="user" to="/">User 12</Link>
+      <Link className="user" to="/">User 18</Link>
     </div>
   );
 };
