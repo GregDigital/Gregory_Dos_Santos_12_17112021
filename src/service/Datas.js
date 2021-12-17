cdd472ed59d09096e126397296f0b8c59dad17a7
GregDigital <68654973+GregDigital@users.noreply.github.com>
2021-12-17 10:34:17 +0100

    build

--- src/service/Datas.js
+++ src/service/Datas.js
@@ -6,11 +6,140 @@ class Datas extends Component {
     super(props);
     //If the server does not respond, change the source in this.state to mock to display the data
     this.state = {
-      source: "api", //choose api or mock
-      users: {},
-      activity: {},
-      perf: {},
-      session: {},
+      source: "mock", //choose api or mock
+      users: {
+        data: {
+          id: 12,
+          userInfos: {
+            firstName: "Karl",
+            lastName: "Dovineau",
+            age: 31,
+          },
+          todayScore: 0.12,
+          keyData: {
+            calorieCount: 1930,
+            proteinCount: 155,
+            carbohydrateCount: 290,
+            lipidCount: 50,
+          },
+        },
+      },
+      activity: {
+        data: {
+          userId: 12,
+          sessions: [
+            {
+              day: "1",
+              kilogram: 80,
+              calories: 240,
+            },
+            {
+              day: "2",
+              kilogram: 80,
+              calories: 220,
+            },
+            {
+              day: "3",
+              kilogram: 81,
+              calories: 280,
+            },
+            {
+              day: "4",
+              kilogram: 81,
+              calories: 290,
+            },
+            {
+              day: "5",
+              kilogram: 80,
+              calories: 160,
+            },
+            {
+              day: "6",
+              kilogram: 78,
+              calories: 162,
+            },
+            {
+              day: "7",
+              kilogram: 76,
+              calories: 390,
+            },
+          ],
+        },
+      },
+      perf: {
+        data: {
+          userId: 12,
+          kind: {
+            1: "cardio",
+            2: "energy",
+            3: "endurance",
+            4: "strength",
+            5: "speed",
+            6: "intensity",
+          },
+          data: [
+            {
+              value: 80,
+              kind: 1,
+            },
+            {
+              value: 120,
+              kind: 2,
+            },
+            {
+              value: 140,
+              kind: 3,
+            },
+            {
+              value: 50,
+              kind: 4,
+            },
+            {
+              value: 200,
+              kind: 5,
+            },
+            {
+              value: 90,
+              kind: 6,
+            },
+          ],
+        },
+      },
+      session: {
+        data: {
+          userId: 12,
+          sessions: [
+            {
+              day: "L",
+              sessionLength: 30,
+            },
+            {
+              day: "M",
+              sessionLength: 23,
+            },
+            {
+              day: "M",
+              sessionLength: 45,
+            },
+            {
+              day: "J",
+              sessionLength: 50,
+            },
+            {
+              day: "V",
+              sessionLength: 0,
+            },
+            {
+              day: "S",
+              sessionLength: 0,
+            },
+            {
+              day: "D",
+              sessionLength: 60,
+            },
+          ],
+        },
+      },
     };
   }
 
